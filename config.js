1b2245356a3369d8e6920374ecc6d44c1699cf82
rubenstein <111526335+reu12th@users.noreply.github.com>
2024-01-27 11:00:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios'
 
 global.owner = [
   ['2348145461867', 'RUBENSTEIN', true],
-  ['6281255369012', 'Ash', true], 
+  ['2348100880822', 'BOT', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "2348145461867" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['2348145461867','2348100880822'] 
+global.prems = ['2348145461867', '2348100880822', '2348108760366']
+global.allowed = ['2348145461867', '2348100880822', '2348108760366']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,16 +50,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴛʜᴇ RUBENSTEIN-BOT'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'RUBENSTEIN┃ᴮᴼᵀ' 
+global.author = '@RUBENSTEIN' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/reu12th\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'https://youtube.com/@reu12th'
+global.fgpyp = 'https://youtube.com/@reu12th'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
